8cf527af70e6360ff5f58e80a465b7c1513ce522
unknown <frieske.jakub@gmail.com>
2022-03-17 21:18:19 +0100

    lab 1

---
+++ ServletsJSPJSTL/src/java/ai/beans/TableBean.java
@@ -0,0 +1,21 @@
+package ai.beans;
+public class TableBean {
+ private String borderTable;
+
+ public TableBean() {
+ }
+
+    /**
+     * @return the borderTable
+     */
+    public String getBorderTable() {
+        return borderTable;
+    }
+
+    /**
+     * @param borderTable the borderTable to set
+     */
+    public void setBorderTable(String borderTable) {
+        this.borderTable = borderTable;
+    }
+}
